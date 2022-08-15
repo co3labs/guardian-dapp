e336d14e5efe08506079a058f69f6414835ef0f1
Keith Frazier <keithers98@gmail.com>
2022-08-15 12:41:31 -0700

    updated favicon

--- src/elements/Header.tsx
+++ src/elements/Header.tsx
@@ -9,24 +9,26 @@ export default function Header() {
   const location = useLocation();
 
   return (
-    <header
-      className={`w-full bg-white flex justify-between items-center shadow-md z-20 absolute transition-transform transform ${
-        location.pathname === '/' ? '-translate-y-full' : ''
-      }`}
-    >
+    <>
       <Link to="/" className="ml-4">
-        <img src={logo} className="w-32" />
+        <img src={logo} className="w-32 absolute z-30 top-2 left-2" />
       </Link>
-      <div className="text-xs grid grid-flow-col gap-4 mr-4 ">
-        {accountId ? (
-          <span className="border-x border-gray-300 p-3 bg-blue-800 text-white">{getShortId(accountId)}</span>
-        ) : (
-          <button className="border-x border-gray-300 p-3 bg-blue-800 text-white" onClick={() => handleConnect()}>
-            Connect Wallet
-          </button>
-        )}
-        <button className="m-auto">{chainId ? networks[chainId] : 'Network'}</button>
-      </div>
-    </header>
+      <header
+        className={`w-full bg-white flex justify-end items-center shadow-md z-20 absolute transition-transform transform ${
+          location.pathname === '/' ? '-translate-y-full' : ''
+        }`}
+      >
+        <div className="text-xs grid grid-flow-col gap-4 mr-4 ">
+          {accountId ? (
+            <span className="border-x border-gray-300 p-3 bg-blue-800 text-white">{getShortId(accountId)}</span>
+          ) : (
+            <button className="border-x border-gray-300 p-3 bg-blue-800 text-white" onClick={() => handleConnect()}>
+              Connect Wallet
+            </button>
+          )}
+          <button className="m-auto">{chainId ? networks[chainId] : 'Network'}</button>
+        </div>
+      </header>
+    </>
   );
 }
