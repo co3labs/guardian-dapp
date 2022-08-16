7d8d0fcda855dbbf59d44403c42bf87ad1114109
Keith Frazier <keithers98@gmail.com>
2022-08-16 13:38:11 -0700

    landing page responsive update

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,13 +4,13 @@ import heros from '../assets/heros.png';
 export default function LandingPage() {
   return (
     <>
-      <div className="flex w-full h-full justify-center items-center">
-        <div className="mr-8">
+      <div className="flex flex-col px-4 md:flex-row w-full h-full justify-center items-center">
+        <div className="md:mr-8">
           <img src={heros} alt="heros" className="w-[32rem]" />
         </div>
-        <div>
-          <h2 className="text-6xl mb-4">Guardians of Your Legacy</h2>
-          <h3 className="text-xl font-light mb-8">Secure your online legacy with guardians.</h3>
+        <div className='flex flex-col items-center md:items-start'>
+          <h2 className="text-3xl md:text-6xl mb-4">Guardians of Your Legacy</h2>
+          <h3 className="text-base md:text-xl font-light mb-8">Secure your online legacy with guardians.</h3>
           <Link to="app/welcome" className="btnPrimary btnBig">
             Enter App
           </Link>
