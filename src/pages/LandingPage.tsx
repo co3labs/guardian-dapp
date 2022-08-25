fd70aaa392871a7e3fcbb16cf7822853ebe452d3
Keith Frazier <keithers98@gmail.com>
2022-08-24 22:43:07 -0700

    updated header spacing LP

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,12 +8,12 @@ export default function LandingPage() {
       <div className="flex w-full h-full flex-col md:flex-row justify-center items-center mt-6">
         <img src={heros} alt="heros" className="w-72 md:w-[28rem]" />
         <div className="flex flex-col items-center font-ubuntu md:items-start pr-8 ml-10">
-          <h2 className="text-3xl md:text-6xl mb-4 font">
-            {' '}
-            <img src={logo} className="w-[32rem]" />
-            <span className='my-12'>of Your Legacy</span>
+          <img src={logo} className="w-[32rem]" />
+          <h2 className="text-3xl md:text-6xl">
+            <span className="text-3xl md:text-6xl">of Your Legacy</span>
           </h2>
-          <h3 className="text-base md:text-xl font-light mb-8">Secure your online legacy with guardians.</h3>
+
+          <h3 className="text-xl md:text-2xl font-light my-12">Secure your online legacy with guardians.</h3>
           <Link to="app/welcome" className="btnPrimary btnBig">
             Enter App
           </Link>
